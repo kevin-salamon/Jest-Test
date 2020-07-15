a14494828283c28cc5a4079ca71eb4ad57159a1d
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-15 12:21:57 -0400

    tried a couple more matchers

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,22 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('object assignment', () => {
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'beer',
+];
+
+test('the shopping list has beer on it', () => {
+  expect(shoppingList).toContain('beer');
+  expect(new Set(shoppingList)).toContain('beer');
+});
